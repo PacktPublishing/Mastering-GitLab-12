3c12df9235bfbc4cb68dd8dd29a64b01a233657d
Joost Evertse <joustie@gmail.com>
2021-05-24 15:51:01 +0200

    Changed provider definition to format of terraform 12+

--- Chapter19/ha-configuration/provider.tf
+++ Chapter19/ha-configuration/provider.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    ansible = {
+      source = "nbering/ansible"
+      version = "1.0.4"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.42.0"
+    }
+  }
+}
 provider "aws" {
     region  = "${var.region}"
 }
